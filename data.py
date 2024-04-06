ad79360dcec39932c1f3c992aeae2a4a8f740747
Bharatm07 <bharatmuthukumar@gmail.com>
2024-04-06 21:04:01 +0530

    first commit

---
+++ data.py
@@ -0,0 +1,128 @@
+question_data = [
+    {
+    "category": "Science: Computers",
+    "type": "boolean",
+    "question": "The binary system uses powers of 2 to denote values.",
+    "correct_answer": "True",
+    "incorrect_answers": [
+        "False"
+    ]
+},
+{
+    "category": "Science: Computers",
+    "type": "boolean",
+    "question": "In computing, 'GUI' stands for 'Graphical User Instruction'.",
+    "correct_answer": "False",
+    "incorrect_answers": [
+        "True"
+    ]
+},
+{
+    "category": "Science: Computers",
+    "type": "boolean",
+    "question": "JavaScript is a case-sensitive language.",
+    "correct_answer": "True",
+    "incorrect_answers": [
+        "False"
+    ]
+},
+{
+    "category": "Science: Computers",
+    "type": "boolean",
+    "question": "A Boolean value of 'false' represents absence of a value.",
+    "correct_answer": "True",
+    "incorrect_answers": [
+        "False"
+    ]
+},
+{
+    "category": "Science: Computers",
+    "type": "boolean",
+    "question": "HTTP stands for 'HyperText Transfer Protocol'.",
+    "correct_answer": "True",
+    "incorrect_answers": [
+        "False"
+    ]
+},
+{
+    "category": "Science: Computers",
+    "type": "boolean",
+    "question": "The programming language 'Java' was first released in 2000.",
+    "correct_answer": "False",
+    "incorrect_answers": [
+        "True"
+    ]
+},
+{
+    "category": "Science: Computers",
+    "type": "boolean",
+    "question": "The 'address bar' in a web browser displays the URL of the current webpage.",
+    "correct_answer": "True",
+    "incorrect_answers": [
+        "False"
+    ]
+},
+{
+    "category": "Science: Computers",
+    "type": "boolean",
+    "question": "RAM stands for 'Random Access Memory'.",
+    "correct_answer": "True",
+    "incorrect_answers": [
+        "False"
+    ]
+},
+{
+    "category": "Science: Computers",
+    "type": "boolean",
+    "question": "HTML stands for 'Hyper Text Markup Language'.",
+    "correct_answer": "True",
+    "incorrect_answers": [
+        "False"
+    ]
+},
+{
+    "category": "Science: Computers",
+    "type": "boolean",
+    "question": "The first mechanical computer was created by Charles Babbage.",
+    "correct_answer": "True",
+    "incorrect_answers": [
+        "False"
+    ]
+},
+{
+    "category": "Science: Computers",
+    "type": "boolean",
+    "question": "The 'delete' key in a keyboard is used to remove files permanently.",
+    "correct_answer": "False",
+    "incorrect_answers": [
+        "True"
+    ]
+},
+{
+    "category": "Science: Computers",
+    "type": "boolean",
+    "question": "Python is a statically typed language.",
+    "correct_answer": "False",
+    "incorrect_answers": [
+        "True"
+    ]
+},
+{
+    "category": "Science: Computers",
+    "type": "boolean",
+    "question": "The TCP/IP protocol was developed for the internet.",
+    "correct_answer": "True",
+    "incorrect_answers": [
+        "False"
+    ]
+},
+{
+    "category": "Science: Computers",
+    "type": "boolean",
+    "question": "The motherboard is also known as the central processing unit.",
+    "correct_answer": "False",
+    "incorrect_answers": [
+        "True"
+    ]
+}
+]
